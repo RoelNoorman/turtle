5d4040677958fe55b9869bce2b72ac520b8d888a
x <trn@un-ihe.org>
2019-03-01 15:33:19 +0100

    fibonacci

--- walk.py
+++ walk.py
@@ -5,13 +5,17 @@ t = turtle.Turtle()
 
 # set colors
 colors = ['red', 'blue']
-
-for j in range(8):
+oldv=0
+newv=1
+tmpv=0
+for j in range(10):
     t.color(colors[j % 2])
-    for i in range(18):
-        t.forward(20)
-        t.left(20)
-    t.right(45)
-
+    tmp = newv
+    newv += oldv
+    oldv = tmp
+    for i in range(15):
+        t.forward(newv)
+        t.left(6)
 
 turtle.mainloop()
+
